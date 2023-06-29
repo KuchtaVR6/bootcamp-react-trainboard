bb4dbfab7ed7fe4f8d9990ee2de1e4e1e98c79b7
KuchtaVR6 <kuchtavr6@gmail.com>
2023-06-29 17:03:13 +0100

    styling and adjustments

--- src/components/NumberOfPassangersSelect.tsx
+++ src/components/NumberOfPassangersSelect.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
 interface NumberOfPassangersSelectArgs {
-    passangerType : string;
+    passengerType : string;
+    onChange : (event : React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const NumberOfPassangersSelect: React.FC<NumberOfPassangersSelectArgs> = ({ passangerType }) => {
+const NumberOfPassengersSelect: React.FC<NumberOfPassangersSelectArgs> = ({ passengerType, onChange }) => {
     return (
         <div>
-            <label htmlFor = { passangerType }>Select the number of {passangerType}: </label>
-            <input id = { passangerType } type = 'number' min = { 0 } max = { 9 } placeholder = { '0' }/>
+            <label htmlFor = { passengerType }>Select the number of {passengerType}: </label>
+            <input id = { passengerType } 
+                className = 'passenger-input'
+                type = 'number' 
+                min = { 0 }
+                max = { 9 } 
+                placeholder = { '0' }
+                onChange = { onChange }
+            />
         </div>
     );
 };
 
-export default NumberOfPassangersSelect;
+export default NumberOfPassengersSelect;
--- src/components/UserInputFormArea.tsx
+++ src/components/UserInputFormArea.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { fetchStations } from '../helpers/ApiCallHelper';
-import NumberOfPassangersSelect from './NumberOfPassangersSelect';
+import NumberOfPassengersSelect from './NumberOfPassangersSelect';
 import StationSelectMenu from './StationSelectMenu';
 
 export type StationInfo = {
@@ -11,19 +11,33 @@ export type StationInfo = {
     id: number;
     crs: string;
     nlc: string;
-    isGrownStation : boolean; 
-    isSilverSeekStation : boolean;
+    isGrownStation: boolean;
+    isSilverSeekStation: boolean;
 }
 
 const UserInputFormArea: React.FC = () => {
 
+    const yearFirstFormat = (date: Date) => {
+        const incorrectFormat = date.toLocaleString('en-GB');
+        const correctFormat = incorrectFormat.replace(/(\d+)\/(\d+)\/(\d+),\W(\d+:\d+):\d+$/, '$3-$2-$1T$4');
+        return correctFormat;
+    };
+
+    const yearFirstToAPIFormat = (date: Date) => {
+        return yearFirstFormat(date) + ':00.000Z';
+    };
+
     const [departureStation, setDepartureStation] = useState<StationInfo>();
     const [destinationStation, setDestinationStation] = useState<StationInfo>();
+    const [selectedDate, setSelectedDate] = useState<string>(yearFirstToAPIFormat(new Date()));
+    const [numberOfAdults, setNumberOfAdults] = useState<number>(0);
+    const [numberOfChildren, setNumberOfChildren] = useState<number>(0);
+
     const [message, setMessage] = useState('Please select both stations');
 
     const [stationList, setStationList] = useState<StationInfo[]>([]);
 
-    const timeRef = useRef<HTMLInputElement>(null); 
+    const timeRef = useRef<HTMLInputElement>(null);
     const firstRender = useRef<boolean>(true);
 
     const handleSubmitStations = () => {
@@ -36,24 +50,34 @@ const UserInputFormArea: React.FC = () => {
     };
 
     useEffect(() => {
-        if (!departureStation || !destinationStation) {
-            setMessage('Please select both stations');
-        } else {
-            if (departureStation.id === destinationStation.id) {
-                setMessage('Destination must be diffrent from the departure');
-            } else {
-                setMessage('');
-            }
-        }
-    }, [departureStation, destinationStation]);
 
-    const stationSort = (stationOne : StationInfo, stationTwo : StationInfo)=>{
+        const selectedTimeAsDate = new Date(selectedDate.slice(0, -3));
+        const minimumTime = new Date(new Date().getTime() - 60*60*1000);
+        
+        if (!departureStation) {
+            setMessage('Please select the departure station.');
+        } else if(!destinationStation) {
+            setMessage('Please select the destination station.');
+        } else if  (departureStation.id === destinationStation.id) {
+            setMessage('Destination must be diffrent from the departure.');
+        } else if (isNaN(selectedTimeAsDate.getTime())) {
+            setMessage('Invalid date selected.');
+        } else if (selectedTimeAsDate < minimumTime) {
+            setMessage('Please select a date in the future.');
+        } else if (numberOfAdults + numberOfChildren <= 0) {
+            setMessage('Please input at least one passenger.');
+        } else {
+            setMessage('');
+        }
+    }, [departureStation, destinationStation, selectedDate, numberOfAdults, numberOfChildren]);
+
+    const stationSort = (stationOne: StationInfo, stationTwo: StationInfo) => {
         return stationOne.name.toLowerCase() > stationTwo.name.toLowerCase() ? 1 : -1;
     };
 
-    const handleStationResponse = (response : Response) => {
+    const handleStationResponse = (response: Response) => {
         response.json().then((body) => {
-            if(body.stations){
+            if (body.stations) {
                 setStationList(body.stations.sort(stationSort));
             }
         });
@@ -61,15 +85,12 @@ const UserInputFormArea: React.FC = () => {
 
     const resetTime = () => {
         if (timeRef.current) {
-            const now = new Date();
-            const incorrectFormat = now.toLocaleString('en-GB');
-            const correctFormat = incorrectFormat.replace(/(\d+)\/(\d+)\/(\d+),\W(\d+:\d+):\d+$/, '$3-$2-$1T$4');
-            timeRef.current.value = correctFormat;
+            timeRef.current.value = yearFirstFormat(new Date());
         }
     };
-    
+
     useEffect(() => {
-        if (firstRender.current){
+        if (firstRender.current) {
             firstRender.current = false;
             fetchStations().then(handleStationResponse);
             resetTime();
@@ -93,15 +114,28 @@ const UserInputFormArea: React.FC = () => {
                         skipTheseStationIDs = { [departureStation?.id] }
                     />
 
-                    <label htmlFor = 'dateSelection'>Departure time:</label>
-                    <input 
-                        className = 'dateSelection'
-                        id = "dateSelection" 
-                        ref = { timeRef } 
-                        type = 'datetime-local'/>
+                    <label htmlFor = 'date-selection'>Departure time:</label>
+                    <div>
+                        <input
+                            className = 'date-selection'
+                            id = "date-selection"
+                            ref = { timeRef }
+                            type = 'datetime-local'
+                            onChange = { (event) => {
+                                setSelectedDate(yearFirstToAPIFormat(new Date(event.target.value)));
+                            } }
+                        />
+                        <button className = 'reset-date-selection' onClick = { resetTime }>Today</button>
+                    </div>
 
-                    <NumberOfPassangersSelect passangerType = 'adults' />
-                    <NumberOfPassangersSelect passangerType = 'children' />
+                    <NumberOfPassengersSelect passengerType = 'adults'
+                        onChange = { (event) => {
+                            setNumberOfAdults(Number(event.target.value));
+                        } } />
+                    <NumberOfPassengersSelect passengerType = 'children'
+                        onChange = { (event) => {
+                            setNumberOfChildren(Number(event.target.value));
+                        } } />
                 </div>
 
                 <div className = 'station-submit-area'>
